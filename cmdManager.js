88c59a4174bb10667f3ad1c2c70182f9d7ab26ac
Skiddled <36399055+SkiddledGitHub@users.noreply.github.com>
2022-06-11 22:52:13 +0700

    [Fix] Fix for error 20012

--- cmdManager.js
+++ cmdManager.js
@@ -22,8 +22,7 @@ const { stdin: input, stdout: output } = require('process');
 const process = require('process');
 const fs = require('fs');
 const rl = readline.createInterface({ input, output });
-const clientID = "413250765629423636";
-const { botAuth } = require('./config.json');
+const { botAuth, botID } = require('./config.json');
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -49,7 +48,7 @@ function cmdMan() {
       console.log(' \x1b[1;32m=> \x1b[1;37mRefreshing (/) commands');
 
       await rest.put(
-        Routes.applicationCommands(clientID),
+        Routes.applicationCommands(botID),
         { body: commands },
       );
 
@@ -66,7 +65,7 @@ function cmdMan() {
       console.log(' \x1b[1;32m=> \x1b[1;37mDestroying (/) commands');
 
       await rest.put(
-        Routes.applicationCommands(clientID),
+        Routes.applicationCommands(botID),
         { body: [] },
       );
 
