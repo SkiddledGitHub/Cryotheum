a487749137098936ac2b4bfb73a185ebce5ca43c
SkiddledGitHub <trucphuong87272@gmail.com>
2022-07-06 11:42:04 +0700

    [Commands > Avatar] Add support for gif avatars

--- commands/avatar.js
+++ commands/avatar.js
@@ -32,61 +32,76 @@ module.exports = {
   	.setDescription('Get avatar from a Discord user.')	
 		.addUserOption((option) => option.setName('target').setDescription('The target user to get the avatar from')),
 	async execute(interaction) {
-
-    // set executor
-		const executor = interaction.member;
-    const executorTag = executor.user.tag;
-
-    // variables
-    var target;
-    var targetTag;
-
-    if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Command initialized by ${executorTag}` }); };
-
-		// set target
-    // no target provided
-		if (!interaction.options.getUser('target') && !interaction.options.getMember('target')) {
-
-      // set executor as target
-      target = executor;
-      targetTag = target.user.tag;
-
-      // if target is in server
-    } else if (interaction.options.getUser('target') && interaction.options.getMember('target')) {
-
-      // set target
-      target = interaction.options.getMember('target');
-      targetTag = target.user.tag;
-    
-      // if target is outside server
-    } else if (interaction.options.getUser('target') && !interaction.options.getMember('target')) {
-
-      // set target
-      target = interaction.options.getUser('target');
-      targetTag = target.tag;
-
-    };
-
-    if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Target set to ${targetTag}` }); };
-
-    // create embed object
-    if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Constructing embed` }); };
-    const avatarEmbed = embedConstructor("avatar", { who: `${targetTag}`, image: `${target.displayAvatarURL({ dynamic: true, size: 1024 })}` })
     	// cooldown management
       if (cooldown.has(interaction.user.id)) {
       await interaction.reply({ embeds: [cooldownEmbed] });
     	} else {
-    		
-        // reply
-        if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Replying with embed` }); };
-      	await interaction.reply({ embeds: [avatarEmbed] });
-      	if (debug) { if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Avatar command succeeded.`, extra: [`Target is ${targetTag}`] }); }; };
 
-      	// cooldown management
-      	cooldown.add(interaction.user.id);
-        setTimeout(() => { cooldown.delete(interaction.user.id); }, cooldownTime);
+        // set executor
+        const executor = { obj: interaction.member, tag: interaction.member.user.tag };
+
+        // variables
+        let target = {};
+
+        if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Command initialized by ${executor.tag}` }); };
+
+      // set target
+      // no target provided
+      if (!interaction.options.getUser('target') && !interaction.options.getMember('target')) {
+
+        // set executor as target
+        target.obj = executor.obj;
+        target.tag = target.obj.user.tag;
+	      if (target.obj.avatar) {
+		      target.avatarHash = target.obj.avatar;
+	      } else {
+		      target.avatarHash = target.obj.user.avatar;
+	      };
+
+      // if target is in server
+      } else if (interaction.options.getUser('target') && interaction.options.getMember('target')) {
+
+        // set target
+        target.obj = interaction.options.getMember('target');
+        target.tag = target.obj.user.tag;
+	      if (target.obj.avatar) {
+		      target.avatarHash = target.obj.avatar;
+	      } else {
+		      target.avatarHash = target.obj.user.avatar;
+	      };
+    
+      // if target is outside server
+      } else if (interaction.options.getUser('target') && !interaction.options.getMember('target')) {
+
+        // set target
+        target.obj = interaction.options.getUser('target');
+        target.tag = target.obj.tag;
+        target.avatarHash = target.obj.avatar;
+
+      };
+
+      if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Target set to ${target.tag}` }); };
+
+      // create embed object
+      if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Constructing embed` }); };
+      let embed;
+
+      if (target.avatarHash.slice(0,2) == 'a_') {
+        embed = embedConstructor("avatar", { who: `${target.tag}`, image: `${target.obj.displayAvatarURL({ format: 'gif', dynamic: true, size: 1024 })}` });
+      } else {
+        embed = embedConstructor("avatar", { who: `${target.tag}`, image: `${target.obj.displayAvatarURL({ dynamic: true, size: 1024 })}` });
+      }
+    		
+      // reply
+      if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Replying with embed` }); };
+      await interaction.reply({ embeds: [embed] });
+      if (debug) { if (debug) { log('genLog', { event: 'Commands > Avatar', content: `Avatar command succeeded.`, extra: [`Target is ${target.tag}`] }); }; };
+
+      // cooldown management
+      cooldown.add(interaction.user.id);
+      setTimeout(() => { cooldown.delete(interaction.user.id); }, cooldownTime);
       	
-        }
+      }
     },
   documentation: {
     name: 'avatar',
