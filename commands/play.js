db6c1a8bc6a530404d3fd269a72734fdc39e78c6
SkiddledGitHub <trucphuong87272@gmail.com>
2022-07-18 19:33:39 +0700

    [Commands > Play] Move back to ytdl for latency issues

--- commands/play.js
+++ commands/play.js
@@ -23,7 +23,9 @@ const voice = require('@discordjs/voice');
 // custom modules
 const { embedConstructor } = require('../lib/embeds.js');
 const { log } = require('../lib/logging.js');
-const invidious = require('../lib/invidious.js');
+// const invidious = require('../lib/invidious.js'); # The Invidious library has been deprecated due to poor performance.
+const yt = require('../lib/yt.js');
+const ytdl = require('ytdl-core');
 
 // data
 const { debug } = require('../config.json');
@@ -37,7 +39,7 @@ module.exports = {
   data: new SlashCommandBuilder()
   .setName('play')
   .setDescription('Play audio to VC from specified search query')
-  .addStringOption((option) => option.setName('query').setDescription('Search query').setRequired(true)),
+  .addStringOption((option) => option.setName('query').setDescription('Query (this can be a YouTube URL or a search query)').setRequired(true)),
   async execute(interaction) {
       // cooldown management
       if (cooldown.has(interaction.user.id)) {
@@ -70,70 +72,109 @@ module.exports = {
       let patient = embedConstructor('playAwait', {})
       await interaction.reply({ embeds: [patient] })
 
-      let searchResults
-      try {
-         searchResults = await invidious.search(query, {type: 'video'})
-      } catch (e) {
-        if (debug) log('genWarn', { event: 'Commands > Play', content: 'Failed.', cause: 'Search failure!', extra: [`${query}`,`${executor.tag}`] })
-        let embed = embedConstructor('playFailed', { reason: e.message, query: query })
-        interaction.editReply({ embeds: [embed] })
-        log('genLog', { event: 'Commands > Play', content: `Done${debug ? '' : ' with suppressed warnings'}.` })
-        return
-      }
+      if (!ytdl.validateURL(query)) {
+        let searchResults
+        try {
+          // searchResults = await invidious.search(query, {type: 'video'}) # Invidious remnant
+          searchResults = await yt.searchVideo(query, 5);
+        } catch (e) {
+          if (debug) log('genWarn', { event: 'Commands > Play', content: 'Failed.', cause: 'Search failure!', extra: [`${query}`,`${executor.tag}`] })
+          let embed = embedConstructor('playFailed', { reason: e.message, query: query })
+          interaction.editReply({ embeds: [embed] })
+          log('genLog', { event: 'Commands > Play', content: `Done${debug ? '' : ' with suppressed warnings'}.` })
+          return
+        } 
 
-      let buttons = new ActionRowBuilder().addComponents(
-        new ButtonBuilder().setLabel('1').setCustomId('0').setStyle(ButtonStyle.Secondary),
-        new ButtonBuilder().setLabel('2').setCustomId('1').setStyle(ButtonStyle.Secondary),
-        new ButtonBuilder().setLabel('3').setCustomId('2').setStyle(ButtonStyle.Secondary),
-        new ButtonBuilder().setLabel('4').setCustomId('3').setStyle(ButtonStyle.Secondary),
-        new ButtonBuilder().setLabel('5').setCustomId('4').setStyle(ButtonStyle.Secondary)
-      )
+        let buttons = new ActionRowBuilder().addComponents(
+          new ButtonBuilder().setLabel('1').setCustomId('0').setStyle(ButtonStyle.Secondary),
+          new ButtonBuilder().setLabel('2').setCustomId('1').setStyle(ButtonStyle.Secondary),
+          new ButtonBuilder().setLabel('3').setCustomId('2').setStyle(ButtonStyle.Secondary),
+          new ButtonBuilder().setLabel('4').setCustomId('3').setStyle(ButtonStyle.Secondary),
+          new ButtonBuilder().setLabel('5').setCustomId('4').setStyle(ButtonStyle.Secondary)
+        )
 
-      if (!searchResults.length == 5) {
-        for (var i = 4; i > (searchResults.length - 1); i--) {
-          buttons.components[i].disabled = true
+        if (!searchResults.length == 5) {
+          for (var i = 4; i > (searchResults.length - 1); i--) {
+            buttons.components[i].disabled = true
+          }
         }
-      }
 
-      let embed = embedConstructor('playSelection', { results: searchResults })
-      if (debug) log('genWarn', { event: 'Commands > Play', content: 'Embed spawned', extra: [`${query}`,`${executor.tag}`] })
-      await interaction.editReply({ embeds: [embed], components: [buttons] })
-      let selected
-      let link
+        let embed = embedConstructor('playSelection', { results: searchResults })
+        if (debug) log('genWarn', { event: 'Commands > Play', content: 'Embed spawned', extra: [`${query}`,`${executor.tag}`] })
+        await interaction.editReply({ embeds: [embed], components: [buttons] })
+        let selected
 
-      // collector
-      const filter = i => i.customId === '0' || i.customId === '1' || i.customId === '2' || i.customId === '3' || i.customId === '4'
-      const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 })
-      if (debug) log('genLog', { event: 'Commands > Play', content: 'Component collector spawned', extra: [`${executor.tag}`] })
+        // collector
+        const filter = i => i.customId === '0' || i.customId === '1' || i.customId === '2' || i.customId === '3' || i.customId === '4'
+        const collector = interaction.channel.createMessageComponentCollector({ filter, time: 60000 })
+        if (debug) log('genLog', { event: 'Commands > Play', content: 'Component collector spawned', extra: [`${executor.tag}`] })
 
-      collector.on('collect', async i => {
-        if (i.user.id === executor.id) {
-          await i.deferUpdate();
-          await i.editReply({ embeds: [patient], components: [] });
-          selected = await invidious.getVideo(searchResults[i.customId].metadata.id)
-          link = selected.video.adaptive.find(item => item.itag === '140').url
-          if (debug) log('genWarn', { event: 'Commands > Play', content: 'Executor selected a video', extra: [`${searchResults[i.customId].metadata.title} by ${searchResults[i.customId].author.name}`,`${executor.tag}`] })
-          let selectedEmbed = embedConstructor('playSelected', { title: searchResults[i.customId].metadata.title, name: searchResults[i.customId].author.name });
-          await i.editReply({ embeds: [selectedEmbed], components: [] })
+        collector.on('collect', async i => {
+          if (i.user.id === executor.id) {
+            await i.deferUpdate()
+            await i.editReply({ embeds: [patient], components: [] })
 
-          if (debug) log('genLog', { event: 'Commands > Play', content: 'Component collector killed' })
-          collector.stop()
-        } else {
-          if (debug) log('genWarn', { event: 'Commands > Ban', content: `Interaction owner mismatch`, extra: [`Expected: ${executor.tag}`, `Got: ${i.user.tag}`] })
-          let notForUserEmbed = embedConstructor('notForUser', {})
-          await i.reply({ embeds: [notForUserEmbed], ephemeral: true })
-        }
-      })
+            /* selected = await invidious.getVideo(searchResults[i.customId].metadata.id) # Invidious remnant
+            link = selected.video.adaptive.find(item => item.itag === '140').url */
 
-      await collector.on('end', collected => {
-        if (selected) {
+            selected = searchResults[i.customId]
+
+            if (debug) log('genWarn', { event: 'Commands > Play', content: 'Executor selected a video', extra: [`${selected.title} by ${selected.author.name}`,`${executor.tag}`] })
+            let selectedEmbed = embedConstructor('playSelected', { title: selected.title, name: selected.author.name, thumbnail: selected.bestThumbnail.url });
+            await i.editReply({ embeds: [selectedEmbed], components: [] })
+
+            if (debug) log('genLog', { event: 'Commands > Play', content: 'Component collector killed' })
+            collector.stop()
+          } else {
+            if (debug) log('genWarn', { event: 'Commands > Ban', content: `Interaction owner mismatch`, extra: [`Expected: ${executor.tag}`, `Got: ${i.user.tag}`] })
+            let notForUserEmbed = embedConstructor('notForUser', {})
+            await i.reply({ embeds: [notForUserEmbed], ephemeral: true })
+          }
+        })
+
+        await collector.on('end', collected => {
+          if (selected) {
+            try {
+
+              let player = voice.createAudioPlayer()
+              let stream = ytdl(selected.url, { filter: 'audioonly', dlChunkSize: 0 })
+              let resource = voice.createAudioResource(stream)
+
+              connection.subscribe(player)
+              player.play(resource)
+
+              log('genLog', { event: 'Commands > Play', content: 'Done.', extra: [`${selected.title} by ${selected.author.name}`,`${executor.tag}`] })
+
+              player.on(voice.AudioPlayerStatus.Idle, () => {
+                let timeoutEmbed = embedConstructor('playTimeout')
+                interaction.channel.send({ embeds: [timeoutEmbed] })
+                connection.destroy()
+                log('genLog', { event: 'Commands > Play', content: 'Connection destroyed.', extra: [`${executor.tag}`] })
+              })
+
+            } catch (e) {
+              log('runtimeErr', { errName: e.name, event: 'Play', content: e.message })
+              let embed = embedConstructor('playUnavailable', { query: query })
+              interaction.followUp({ embeds: [embed] })
+              connection.destroy()
+              return
+            }
+          }
+        })
+      } else {
+        try {
+
+          let selected = await ytdl.getInfo(query);
+          selected = selected.videoDetails;
+          let thumbnail = selected.thumbnails.pop().url;
           let player = voice.createAudioPlayer()
-          let successEmbed = embedConstructor('playSuccess', { title: selected.metadata.title, name: selected.author.name })
-          let resource = voice.createAudioResource(link)
+          let stream = ytdl(selected.video_url, { filter: 'audioonly', dlChunkSize: 0 })
+          let resource = voice.createAudioResource(stream)
+
           connection.subscribe(player)
           player.play(resource)
-          interaction.followUp({ embeds: [successEmbed] })
-          log('genLog', { event: 'Commands > Play', content: 'Done.', extra: [`${selected.metadata.title} by ${selected.author.name}`,`${executor.tag}`] })
+
+          log('genLog', { event: 'Commands > Play', content: 'Done.', extra: [`${selected.title} by ${selected.author.name}`,`${executor.tag}`] })
 
           player.on(voice.AudioPlayerStatus.Idle, () => {
             let timeoutEmbed = embedConstructor('playTimeout')
@@ -141,8 +182,15 @@ module.exports = {
             connection.destroy()
             log('genLog', { event: 'Commands > Play', content: 'Connection destroyed.', extra: [`${executor.tag}`] })
           })
+              
+        } catch (e) {
+          log('runtimeErr', { errName: e.name, event: 'Play', content: e.message })
+          let embed = embedConstructor('playUnavailable', { query: query })
+          interaction.followUp({ embeds: [embed] })
+          connection.destroy()
+          return
         }
-      })
+      }
 
       // cooldown management
     	cooldown.add(executor.id)
